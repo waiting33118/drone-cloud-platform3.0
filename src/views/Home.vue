c8889379e7c4f1b26f6b719a30b5c5ceeea059a7
Tony <waiting33118@gmail.com>
2020-11-28 23:35:06 +0800

    feat: create home view page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="panel">
+    <ControlPanel
+      class="control"
+      :coords="coords"
+    />
+    <Mapbox
+      id="map"
+      @getCurrentPosition="getCurrentPosition"
+    />
+    <Stream class="stream" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { reactive } from '@vue/composition-api'
+import ControlPanel from './../components/ControlPanel'
+import Mapbox from './../components/Mapbox'
+import Stream from './../components/Stream'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ControlPanel,
+    Mapbox,
+    Stream
+  },
+  setup () {
+    const coords = reactive({
+      lng: 0,
+      lat: 0
+    })
+
+    /**
+     * @param lng number
+     * @param lat number
+     */
+    const getCurrentPosition = (lng, lat) => {
+      coords.lng = lng
+      coords.lat = lat
+    }
+
+    return {
+      coords,
+      getCurrentPosition
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .panel{
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(7,minmax(auto,500px));
+    grid-template-rows: repeat(2,minmax(250px,1fr));
+    > .control {
+      grid-column: 1 / 2;
+      grid-row: 1;
+      border: 2px solid gainsboro;
+    }
+    > #map {
+      grid-column: 2 / 8;
+      grid-row: 1 / span 2;
+      border: 2px solid gainsboro;
+    }
+    > .stream {
+      width: 500px;
+      grid-column: 1 / 2;
+      grid-row: 2;
+      border: 2px solid gainsboro;
+    }
+  }
+</style>
