6d0c029bd15109fcd3bc87caccdc0e53420e0aa9
Tony <waiting33118@gmail.com>
2020-11-30 17:20:21 +0800

    feat: edit API url(google cloud app engine)

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 /**
  * custom axios config
  * ```
- * baseURL: 'http://127.0.0.1:3030/api/v1'
+ * baseURL: 'https://drone-cloud-295410.df.r.appspot.com/api/v1/
  * ```
  */
 export const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3030/api/v1'
+  baseURL: 'https://drone-cloud-295410.df.r.appspot.com/api/v1/'
 })
