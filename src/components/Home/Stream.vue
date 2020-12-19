cf884dffdfb65265b19b631a5f21e6557f645d89
Tony Chung <waiting33118@gmail.com>
2020-12-19 22:13:11 +0800

    feat: create Stream [component]

---
+++ src/components/Home/Stream.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- autoplay -->
+  <!-- controls -->
+  <video
+    loop
+    muted
+  >
+    <source
+      src="https://i.imgur.com/zQJGZvg.mp4"
+      type="video/mp4"
+    >
+  </video>
+</template>
+
+<script>
+export default {
+  name: 'Stream'
+}
+</script>
+
+<style lang="scss" scoped>
+  video {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+</style>
