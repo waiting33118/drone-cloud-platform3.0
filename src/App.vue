26904dab56661d7157fbc7ad5d592d5727312f79
Tony Chung <waiting33118@gmail.com>
2020-12-19 22:10:47 +0800

    feat:add html,body element height

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,8 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-#app {
+#app,html,body {
   font-family: 'Roboto',sans-serif;
+  height: 100%;
 }
 </style>
