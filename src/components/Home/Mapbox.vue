a9bf9e977a843df60c4d2cc68bbe66311f564c6e
Tony Chung <waiting33118@gmail.com>
2020-12-19 22:13:02 +0800

    feat: create Mapbox [component]

---
+++ src/components/Home/Mapbox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="map" />
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+import { onMounted } from 'vue'
+export default {
+  name: 'Mapbox',
+  setup () {
+    onMounted(() => {
+      mapboxgl.accessToken = 'pk.eyJ1Ijoid2FpdGluZzMzMTE4IiwiYSI6ImNrZDVlZWp6MjFxcXQyeHF2bW0xenU4YXoifQ.iGfojLdouAjsovJuRxjYVA'
+      /**
+       * Create map instance & binding DOM Element
+       */
+      const map = new mapboxgl.Map({
+        style: 'mapbox://styles/waiting33118/ckdfkx3t10k9w1irkp8anuy39',
+        center: [121, 24],
+        zoom: 17,
+        pitch: 55,
+        bearing: 0,
+        antialias: true,
+        container: 'map'
+      })
+      console.log(map)
+    })
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
