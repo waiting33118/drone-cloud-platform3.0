23fd6be6553eafa7119025dddfeaff326b545bfb
Tony Chung <waiting33118@gmail.com>
2020-12-19 22:13:26 +0800

    feat: create Range [component]

---
+++ src/components/ControlPanel/Range.vue
@@ -0,0 +1,37 @@
+<template>
+  <label
+    for="range"
+    class="form-label"
+  ><slot /></label>
+  <input
+    id="range"
+    v-model="rangeValue"
+    type="range"
+    class="form-range"
+  >
+  <input
+    class="form-control"
+    type="text"
+    placeholder="Disabled readonly input"
+    aria-label="Disabled input example"
+    disabled
+    readonly
+  >
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'Range',
+  setup (prop, { emit }) {
+    const rangeValue = ref(0)
+    return {
+      rangeValue
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
