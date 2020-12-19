95dc4540135777747c59e0092a3044b22699c46b
Tony Chung <waiting33118@gmail.com>
2020-12-19 22:13:43 +0800

    feat: create SwitchButton [component]

---
+++ src/components/ControlPanel/SwitchButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form-check form-switch">
+    <input
+      id="flexSwitchCheckDefault"
+      v-model="buttonValue"
+      class="form-check-input"
+      type="checkbox"
+      @change="changeEmit"
+    >
+    <label
+      class="form-check-label"
+      for="flexSwitchCheckDefault"
+    >
+      <slot />
+    </label>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'SwitchButton',
+  emits: ['handleButtonEmit'],
+  setup (props, { emit }) {
+    const buttonValue = ref(false)
+    const changeEmit = () => emit('handleButtonEmit', buttonValue.value)
+    return {
+      buttonValue,
+      changeEmit
+    }
+  }
+}
+</script>
