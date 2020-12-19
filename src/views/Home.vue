2c131a988281c84991f7f6d6ae8abe9102b1e039
Tony Chung <waiting33118@gmail.com>
2020-12-19 22:14:34 +0800

    feat: edit home [VIEWS], import components & set grid style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid px-0">
+    <ControlPanel class="control-panel" />
+    <Stream class="stream" />
+    <Mapbox class="mapbox" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Mapbox from '@/components/Home/Mapbox.vue'
+import ControlPanel from '@/components/Home/ControlPanel.vue'
+import Stream from '@/components/Home/Stream.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Mapbox,
+    ControlPanel,
+    Stream
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container-fluid{
+  height: calc(100% - 58px);  // 58px = navbar height
+  display: grid;
+  grid-template-columns: repeat(7,minmax(auto,500px));
+  grid-template-rows: repeat(2,minmax(250px,1fr));
+    >.control-panel{
+      grid-column: 1 / 2;
+      grid-row: 1;
+      border: 2px solid gainsboro;
+    }
+    >.stream{
+      width: 500px;
+      grid-column: 1 / 2;
+      grid-row: 2;
+      border: 2px solid gainsboro;
+    }
+    >.mapbox{
+      grid-column: 2 / 8;
+      grid-row: 1 / span 2;
+      border: 2px solid gainsboro;
+      position: relative;
+    }
+  @media screen and (max-width: 992px) {
+    grid-template-columns: repeat(6,minmax(auto,350px));
+    >.control-panel{
+      grid-column: 1 / 2;
+      grid-row: 1;
+      border: 2px solid gainsboro;
+    }
+    >.stream{
+      width: 350px;
+      grid-column: 1 / 2;
+      grid-row: 2;
+      border: 2px solid gainsboro;
+    }
+    >.mapbox{
+      grid-column: 2 / 7;
+      grid-row: 1 / span 2;
+      border: 2px solid gainsboro;
+      position: relative;
+    }
+  }
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3,minmax(350px,1fr));
+    >.control-panel{
+      grid-column: 1 ;
+      grid-row: 1;
+      border: 2px solid gainsboro;
+    }
+    >.stream{
+      width: 100%;
+      grid-column: 1 ;
+      grid-row: 2;
+      border: 2px solid gainsboro;
+    }
+    >.mapbox{
+      grid-column: 1;
+      grid-row: 3;
+      border: 2px solid gainsboro;
+      position: relative;
+    }
+  }
+}
+</style>
