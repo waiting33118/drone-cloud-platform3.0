0584f4ee2d0eff4ce7e7700ecf2ccf730c88c21d
Tony Chung <waiting33118@gmail.com>
2020-12-20 14:29:07 +0800

    feat: edit style & add emits

--- src/components/ControlPanel/Range.vue
+++ src/components/ControlPanel/Range.vue
@@ -8,12 +8,15 @@
     v-model="rangeValue"
     type="range"
     class="form-range"
+    step="0.5"
+    min="1"
+    max="100"
+    @change="changeEmit"
   >
   <input
-    class="form-control"
+    class="form-control text-center w-50 mx-auto"
     type="text"
-    placeholder="Disabled readonly input"
-    aria-label="Disabled input example"
+    :placeholder="rangeValue"
     disabled
     readonly
   >
@@ -23,10 +26,13 @@
 import { ref } from 'vue'
 export default {
   name: 'Range',
+  emits: ['handleRangeEmit'],
   setup (prop, { emit }) {
-    const rangeValue = ref(0)
+    const rangeValue = ref(3)
+    const changeEmit = () => emit('handleRangeEmit', rangeValue.value)
     return {
-      rangeValue
+      rangeValue,
+      changeEmit
     }
   }
 }
