3a9a28660581a0b6eda21d0f88dbd95d868a5bc4
Tony Chung <waiting33118@gmail.com>
2020-12-20 14:29:34 +0800

    feat: create Form [component]

---
+++ src/components/ControlPanel/Form.vue
@@ -0,0 +1,59 @@
+<template>
+  <form class="input-group">
+    <FontAwesomeIcon
+      v-model="coordinate.longitude"
+      class="me-2 my-auto"
+      :icon="mapMarkAlt"
+      style="color:blue"
+    />
+    <input
+      id="longitude"
+      type="text"
+      class="form-control text-center"
+      disabled
+      readonly
+    >
+    <input
+      v-model="coordinate.latitude"
+      type="text"
+      class="form-control text-center"
+      disabled
+      readonly
+    >
+    <button
+      id="button-addon2"
+      class="btn btn-outline-primary"
+      type="submit"
+      @click.prevent=""
+    >
+      GOTO
+    </button>
+  </form>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { computed, reactive } from 'vue'
+export default {
+  name: 'Form',
+  components: {
+    FontAwesomeIcon
+  },
+  setup () {
+    const coordinate = reactive({
+      longitude: '-',
+      latitude: '-'
+    })
+
+    /**
+     * FontAwesome implementation
+     */
+    const mapMarkAlt = computed(() => faMapMarkedAlt)
+    return {
+      coordinate,
+      mapMarkAlt
+    }
+  }
+}
+</script>
