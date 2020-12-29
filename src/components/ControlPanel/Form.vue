c78cfc7bd06c6a59fb48e56ab68e6a67d6705218
Tony Chung <waiting33118@gmail.com>
2020-12-29 16:48:48 +0800

    feat: Gps viewer received props

--- src/components/ControlPanel/Form.vue
+++ src/components/ControlPanel/Form.vue
@@ -5,51 +5,55 @@
       :icon="mapMarkAlt"
       style="color:blue"
     />
+    <span class="my-auto mx-1">Target GPS</span>
     <input
       id="longitude"
-      v-model="coordinate.longitude"
+      v-model="coordinate.lng"
       type="text"
       class="form-control text-center"
       disabled
       readonly
     >
     <input
-      v-model="coordinate.latitude"
+      v-model="coordinate.lat"
       type="text"
       class="form-control text-center"
       disabled
       readonly
     >
-    <button
-      id="button-addon2"
-      class="btn btn-outline-primary"
-      type="submit"
-      @click.prevent=""
-    >
-      GOTO
-    </button>
   </form>
 </template>
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
-import { computed, reactive } from 'vue'
+import { computed, reactive, watch } from 'vue'
 export default {
   name: 'Form',
   components: {
     FontAwesomeIcon
   },
-  setup () {
+  props: {
+    targetGps: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  setup (prop) {
     const coordinate = reactive({
-      longitude: '-',
-      latitude: '-'
+      lng: '-',
+      lat: '-'
     })
 
     /**
      * FontAwesome implementation
      */
     const mapMarkAlt = computed(() => faMapMarkedAlt)
+
+    watch(() => prop.targetGps, newCoords => {
+      Object.assign(coordinate, newCoords)
+    })
+
     return {
       coordinate,
       mapMarkAlt
