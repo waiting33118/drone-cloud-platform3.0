7d7ab5646762d72f26b2aa0a4c6285e207716cd8
Tony Chung <waiting33118@gmail.com>
2021-01-24 21:13:35 +0800

    feat: create vuex of Drone state

---
+++ src/store/Drone/state.js
@@ -0,0 +1,30 @@
+export default {
+  info: {
+    timestamp: '',
+    deviceId: '',
+    longitude: '',
+    latitude: '',
+    heading: '',
+    relativeAltitude: '',
+    airSpeed: '',
+    flightMode: '',
+    isArm: false,
+    pitch: '',
+    roll: '',
+    yaw: '',
+    voltage: '',
+    percentage: '',
+    gpsCount: '',
+    hpop: ''
+  },
+  defaultParams: {
+    altitude: 3,
+    speed: 3,
+    targetLongitude: '',
+    targetLatitude: '',
+    userLongitude: '',
+    userLatitude: '',
+    tmpCoords: []
+  },
+  logs: []
+}
