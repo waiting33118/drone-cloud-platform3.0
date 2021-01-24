d7f4a3e96a6ff6217de5fd8973c250c26a7d3275
Tony Chung <waiting33118@gmail.com>
2021-01-24 21:16:37 +0800

    refactor: use vuex to store logs

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,14 @@
 import { io } from 'socket.io-client'
+import { useStore } from 'vuex'
 
-export const startConnections = () => {
-  const domain = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3030' : 'https://drone-cloud-295410.df.r.appspot.com'
-  const socket = io(domain)
-  socket.on('disconnect', reason => console.warn(reason))
-  return socket
+export const socketInit = () => {
+  const DOMAIN = process.env.NODE_ENV === 'development'
+    ? 'http://127.0.0.1:3030'
+    : 'https://drone-cloud-295410.df.r.appspot.com'
+
+  const socket = io(DOMAIN)
+  const store = useStore()
+
+  socket.on('connect', () => store.dispatch('Drone/connect', socket.id))
+  socket.on('disconnect', reason => store.dispatch('Drone/disconnect', reason))
 }
