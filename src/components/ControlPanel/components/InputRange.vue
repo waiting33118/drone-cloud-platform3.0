7963629f20913be744e04b6cef710e7f61bd7460
Tony Chung <waiting33118@gmail.com>
2021-01-25 11:18:42 +0800

    feat: create component- inputRange for SPEED,YAW,GIMBA

---
+++ src/components/ControlPanel/components/InputRange.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex flex-column align-items-center px-2 py-2">
+    <label for="inputRange">
+      <slot />
+    </label>
+
+    <input
+      id="inputRange"
+      type="range"
+      class="form-range"
+    >
+    <input
+      type="text"
+      class="form-control text-center"
+      value="1"
+      disabled
+      readonly
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputRange',
+  setup () {
+
+  }
+}
+</script>
