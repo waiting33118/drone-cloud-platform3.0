6487ce230151ab565ad77f7739eb7bcc17427790
Tony Chung <waiting33118@gmail.com>
2021-01-25 11:23:37 +0800

    refactor: use vuex instead props/emits

--- src/components/Mapbox/DroneInformation.vue
+++ src/components/Mapbox/DroneInformation.vue
@@ -1,104 +1,33 @@
 <template>
   <ul class="drone-info">
-    <li>{{ `TimeStamp: ${droneSource.timestamp}` }}</li>
-    <li>{{ `Device ID: ${droneSource.deviceId}` }}</li>
-    <li>{{ `Longitude: ${droneSource.lng}` }}</li>
-    <li>{{ `Latitude: ${droneSource.lat}` }}</li>
-    <li>{{ `Heading: ${droneSource.heading}` }}</li>
-    <li>{{ `Altitude: ${droneSource.relativeAlt}` }}</li>
-    <li>{{ `AirSpeed: ${droneSource.airSpeed}` }}</li>
-    <li>{{ `FlightMode: ${droneSource.flightMode}` }}</li>
-    <li>{{ `ARM status: ${droneSource.isArmed === '0'? 'DISARM' : 'ARM'}` }}</li>
-    <li>{{ `Attitude: Pitch: ${droneSource.pitch} Roll: ${droneSource.roll} Yaw: ${droneSource.yaw}` }}</li>
-    <li>{{ `Voltage: ${droneSource.voltage} V` }}</li>
-    <li>{{ `Battery%: ${droneSource.percentage} %` }}</li>
-    <li>{{ `GPS Count: ${droneSource.gpsCount}` }}</li>
-    <li>{{ `HPOP: ${droneSource.hpop}` }}</li>
+    <li>{{ `TimeStamp: ${info.timestamp}` }}</li>
+    <li>{{ `Device ID: ${info.deviceId}` }}</li>
+    <li>{{ `Longitude: ${info.longitude}` }}</li>
+    <li>{{ `Latitude: ${info.latitude}` }}</li>
+    <li>{{ `Heading: ${info.heading}` }}</li>
+    <li>{{ `Altitude: ${info.relativeAltitude}` }}</li>
+    <li>{{ `AirSpeed: ${info.airSpeed}` }}</li>
+    <li>{{ `FlightMode: ${info.flightMode}` }}</li>
+    <li>{{ `ARM status: ${info.isArmed ? 'ARM' : 'DISARM'}` }}</li>
+    <li>{{ `Attitude: Pitch: ${info.pitch} Roll: ${info.roll} Yaw: ${info.yaw}` }}</li>
+    <li>{{ `Voltage: ${info.voltage} V` }}</li>
+    <li>{{ `Battery%: ${info.percentage} %` }}</li>
+    <li>{{ `GPS Count: ${info.gpsCount}` }}</li>
+    <li>{{ `HPOP: ${info.hpop}` }}</li>
   </ul>
 </template>
 
 <script>
-import { reactive, watch } from 'vue'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 export default {
   name: 'DroneInformation',
-  props: {
-    droneInfo: {
-      type: Object,
-      default: () => ({})
-    }
-  },
-  setup (prop) {
-    const droneSource = reactive({
-      timestamp: '---',
-      deviceId: '---',
-      lng: 0,
-      lat: 0,
-      heading: 0,
-      relativeAlt: 0,
-      airSpeed: 0,
-      flightMode: '---',
-      isArmed: '---',
-      pitch: 0,
-      roll: 0,
-      yaw: 0,
-      voltage: 0,
-      percentage: 0,
-      gpsCount: 0,
-      hpop: '---'
-    })
-
-    watch(() => prop.droneInfo, newValue => {
-      const {
-        timestamp,
-        device_id: deviceId,
-        location: {
-          lng,
-          lat,
-          heading,
-          relative_alt: relativeAlt
-        },
-        speed: { air_speed: airSpeed },
-        heartbeat: {
-          flight_mode: flightMode,
-          is_armed: isArmed
-        },
-        attitude: {
-          pitch,
-          roll,
-          yaw
-        },
-        battery: {
-          voltage,
-          percentage
-        },
-        gps_status: {
-          gps_count: gpsCount,
-          hpop
-        }
-      } = newValue
-
-      Object.assign(droneSource, {
-        timestamp,
-        deviceId,
-        lng,
-        lat,
-        heading,
-        relativeAlt,
-        airSpeed,
-        flightMode,
-        isArmed,
-        pitch,
-        roll,
-        yaw,
-        voltage,
-        percentage,
-        gpsCount,
-        hpop
-      })
-    })
+  setup () {
+    const store = useStore()
+    const info = computed(() => store.getters['Drone/getDroneInfo'])
 
     return {
-      droneSource
+      info
     }
   }
 }
