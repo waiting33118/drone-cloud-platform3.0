7f7ce3997bd45f7ae78b69e8f5e8b707bde83b6e
Tony Chung <waiting33118@gmail.com>
2021-01-25 11:17:29 +0800

    feat: create component- inputNumber for ALTITUDE

---
+++ src/components/ControlPanel/components/InputNumber.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex flex-column align-items-center px-2 py-2">
+    <label for="inputNumber">
+      <slot />
+    </label>
+    <input
+      id="inputNumber"
+      type="number"
+      class="form-control text-center mt-4"
+      min="1"
+      max="100"
+      step="0.5"
+    >
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'InputNumber',
+  setup () {
+  }
+}
+</script>
