0714b14ea3c04c27ca6465ab83c7eae041fd6c71
Tony Chung <waiting33118@gmail.com>
2021-01-25 11:13:57 +0800

    feat: create component-tab camera

---
+++ src/components/ControlPanel/tabCamera.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    id="camera"
+    class="tab-pane fade"
+  >
+    <div class="tab-container">
+      <div class="d-flex flex-column justify-content-center p-2">
+        <InputRange>
+          <FontAwesomeIcon :icon="video" />
+          Gimbal Front & Back
+        </InputRange>
+
+        <InputRange>
+          <FontAwesomeIcon :icon="video" />
+          Gimbal Left & Right
+        </InputRange>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faVideo } from '@fortawesome/free-solid-svg-icons'
+import InputRange from '@/components/ControlPanel/components/InputRange.vue'
+import { computed } from 'vue'
+export default {
+  name: 'TabCamera',
+  components: {
+    FontAwesomeIcon,
+    InputRange
+  },
+  setup () {
+    const video = computed(() => faVideo)
+
+    return {
+      video
+    }
+  }
+}
+</script>
