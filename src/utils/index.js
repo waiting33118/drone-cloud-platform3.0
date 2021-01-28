95d59afc344c2372a84676b3d02f5cddc8353ce5
Tony Chung <waiting33118@gmail.com>
2021-01-28 09:40:23 +0800

    feat: export axios instance of utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,10 @@ import { socketInit } from './socket'
 import { logFormat } from './logFormatter'
 import { fetchLocation } from './fetchLocation'
 import { gotoMissionConfirm } from './sweetAlert'
+import { instance } from './axios'
 
 export const useSocket = socketInit
 export const uselogFormatter = logFormat
 export const getUserLocation = fetchLocation
 export const useGotoMissionConfirm = gotoMissionConfirm
+export const customAxios = instance
