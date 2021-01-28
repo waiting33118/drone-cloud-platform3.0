b8a08f8a655af4cf3310c9a87fc64690d2291ee1
Tony Chung <waiting33118@gmail.com>
2021-01-28 14:52:06 +0800

    feat: create message parse feature

--- src/utils/logFormatter.js
+++ src/utils/logFormatter.js
@@ -1,3 +1,5 @@
+import { useAlert } from '.'
+
 /**
  *  Combine timestamp & message
  * @param {string} message
@@ -6,4 +8,19 @@ export const logFormat = (message) => {
   const timestamp = new Date().toLocaleString()
   const log = `${timestamp}- ${message}`
   return log
+}
+
+/**
+ * Parse message is Success or Failed
+ * @param {string | null} cmd For ACK only
+ * @param {string} message
+ */
+export const messageParse = (cmd, message) => {
+  if (message.includes('FAILED') || message.includes('ERROR')) {
+    if (cmd) { message = cmd }
+    useAlert(`${message}`)
+    return
+  }
+  if (cmd) { message = cmd }
+  useAlert(`${message}`, 'success')
 }
