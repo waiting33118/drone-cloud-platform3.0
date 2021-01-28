5853582f63cd84a4735fba029812982ae90937f2
Tony Chung <waiting33118@gmail.com>
2021-01-28 13:11:11 +0800

    refactor: refactor serveral api feature

--- src/api/index.js
+++ src/api/index.js
@@ -97,72 +97,27 @@ export const changeFlightMode = async (mode) => {
   }
 }
 
-// /**
-//  *  Drone command - Change flight mode
-//  * @param {string} mode Drone's flight mode
-//  */
-// async changeFlightMode (mode) {
-//   try {
-//     await useDroneAPI.post('/changeflightmode', { mode })
-//   } catch (error) {
-//     alert({ title: error.message })
-//   }
-// }
-// /**
-//  * Drone command - Let the drone fly to destination
-//  * @param {number} lng Target longitude
-//  * @param {number} lat Target latitude
-//  * @param {number} altitude Drone's altitude
-//  */
-// async goto (lng, lat, altitude) {
-//   try {
-//     await useDroneAPI.post('/goto', { lng, lat, altitude })
-//   } catch (error) {
-//     alert({ title: error.message })
-//   }
-// }
-// /**
-//  * Drone command - Change flying speed
-//  * @param {number} speed Drone's speed
-//  */
-// async changeSpeed (speed) {
-//   try {
-//     await useDroneAPI.post('/changespeed', { speed })
-//   } catch (error) {
-//     alert({ title: error.message })
-//   }
-// }
-// /**
-//  * Drone command - Change heading (nose direction)
-//  * @param {number} angle Drone's heading
-//  */
-// async changeYaw (angle) {
-//   try {
-//     await useDroneAPI.post('/changeyaw', { angle })
-//   } catch (error) {
-//     alert({ title: error.message })
-//   }
-// }
-// /**
-//  *  Drone command - Command servo action
-//  * @param {string} action Servo action(UP,DOWN,STOP)
-//  */
-// async servoControl (action) {
-//   try {
-//     await useDroneAPI.post('/servocontrol', { action })
-//   } catch (error) {
-//     alert({ title: error.message })
-//   }
-// }
-// /**
-//  *  Drone command - Change gimbal angle
-//  * @param {string} cmd Gimbal action(x axis,y axis)
-//  * @param {number} pwm Angle(pwm value)
-//  */
-// async gimbalControl (cmd, pwm) {
-//   try {
-//     await useDroneAPI.post('/gimbalcontrol', { cmd, pwm })
-//   } catch (error) {
-//     alert({ title: error.message })
-//   }
-// }
+/**
+ *  Drone command - Command servo action
+ * @param {string} action Servo action(UP,DOWN,STOP)
+ */
+export const servoControl = async (action) => {
+  try {
+    await customAxios.post('/servocontrol', { action })
+  } catch (error) {
+    console.log(error.response)
+  }
+}
+
+/**
+ *  Drone command - Change gimbal angle
+ * @param {string} cmd Gimbal action(x axis,y axis)
+ * @param {number} pwm Angle(pwm value)
+ */
+export const gimbalControl = async (cmd, pwm) => {
+  try {
+    await customAxios.post('/gimbalcontrol', { cmd, pwm })
+  } catch (error) {
+    console.log(error.response)
+  }
+}
