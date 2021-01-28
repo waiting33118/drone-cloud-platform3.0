984ec2e9f00d5678d6268f1a85660ebc59a58c9e
Tony Chung <waiting33118@gmail.com>
2021-01-28 09:30:45 +0800

    feat: create input groups component of targetGPS

---
+++ src/components/ControlPanel/components/InputTargetGps.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="d-flex justify-content-center px-2 py-2">
+    <form class="input-group d-flex align-items-center">
+      <FontAwesomeIcon
+        :icon="mapMark"
+        class="me-2"
+        style="color:blue"
+      />
+      <span class="input-group-text">Lng</span>
+      <input
+        v-model="coords.lng"
+        type="text"
+        class="form-control text-center"
+        disabled
+        readonly
+      >
+      <span class="input-group-text">Lat</span>
+      <input
+        v-model="coords.lat"
+        type="text"
+        class="form-control text-center"
+        disabled
+        readonly
+      >
+    </form>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+export default {
+  name: 'InputTargetGps',
+  components: {
+    FontAwesomeIcon
+  },
+  setup () {
+    const store = useStore()
+    const mapMark = computed(() => faMapMarkedAlt)
+    const coords = computed(() => store.getters['Drone/getTargetGps'])
+    return {
+      mapMark,
+      coords
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
