426692e8b4472f68dd81cb9d92d6295e2898e6b8
Tony Chung <waiting33118@gmail.com>
2021-01-28 14:53:01 +0800

    feat: export utils features

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,13 @@
 import { socketInit } from './socket'
-import { logFormat } from './logFormatter'
+import { logFormat, messageParse } from './logFormatter'
 import { fetchLocation } from './fetchLocation'
-import { gotoMissionConfirm } from './sweetAlert'
+import { alert, gotoMissionConfirm } from './sweetAlert'
 import { instance } from './axios'
 
 export const useSocket = socketInit
 export const uselogFormatter = logFormat
+export const useMessageParse = messageParse
 export const getUserLocation = fetchLocation
 export const useGotoMissionConfirm = gotoMissionConfirm
+export const useAlert = alert
 export const customAxios = instance
