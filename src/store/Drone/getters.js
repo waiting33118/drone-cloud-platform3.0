bfb6cc6b6ac5b2359f68d5bef3c6384b3236c38a
Tony Chung <waiting33118@gmail.com>
2021-01-28 09:42:53 +0800

    feat: create serveral getters of Drone getters

--- src/store/Drone/getters.js
+++ src/store/Drone/getters.js
@@ -3,11 +3,52 @@ export default {
     return state.logs
   },
 
-  getTmpCoords (state) {
-    return state.defaultParams.tmpCoords
-  },
-
   getDroneInfo (state) {
     return state.info
+  },
+
+  getDronePropsStatus (state) {
+    return state.info.isArm
+  },
+
+  getFlightAltitude (state) {
+    return state.info.relativeAltitude
+  },
+
+  getCurrentYaw (state) {
+    return state.info.heading
+  },
+
+  getCurrentFlightMode (state) {
+    return state.info.flightMode
+  },
+
+  getCurrentGps (state) {
+    const coords = {
+      lng: state.info.longitude,
+      lat: state.info.latitude
+    }
+    return coords
+  },
+
+  getFlightSpeed (state) {
+    return state.parameters.speed
+  },
+
+  getTmpCoords (state) {
+    return state.parameters.tmpCoords
+  },
+
+  getCurrentAltitude (state) {
+    return state.parameters.altitude
+  },
+
+  getTargetGps (state) {
+    const coords = {
+      lng: state.parameters.targetLongitude,
+      lat: state.parameters.targetLatitude
+    }
+    return coords
   }
+
 }
