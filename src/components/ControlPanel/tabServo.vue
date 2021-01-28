5f0fed18b141155e19068aff1be680165200dce0
Tony Chung <waiting33118@gmail.com>
2021-01-28 13:07:57 +0800

    feat: create change servo feature

--- src/components/ControlPanel/tabServo.vue
+++ src/components/ControlPanel/tabServo.vue
@@ -4,20 +4,29 @@
     class="tab-pane fade"
   >
     <div class="tab-container">
-      <div class="d-flex flex-column justify-content-center p-2">
-        <button class="btn btn-primary p-4 mb-2 fs-5">
+      <div class="d-flex flex-column justify-content-evenly p-2">
+        <button
+          class="btn btn-primary p-4 mb-2 fs-5"
+          @click="handleServo('UP')"
+        >
           SERVO UP
           <FontAwesomeIcon :icon="box" />
           <FontAwesomeIcon :icon="arrowUp" />
         </button>
 
-        <button class="btn btn-primary p-4 mb-2 fs-5">
+        <button
+          class="btn btn-primary p-4 mb-2 fs-5"
+          @click="handleServo('DOWN')"
+        >
           SERVO DOWN
           <FontAwesomeIcon :icon="box" />
           <FontAwesomeIcon :icon="arrowDown" />
         </button>
 
-        <button class="btn btn-primary bg-danger p-4 mb-2 fs-5">
+        <button
+          class="btn btn-primary bg-danger p-4 mb-2 fs-5"
+          @click="handleServo('STOP')"
+        >
           SERVO STOP
           <FontAwesomeIcon :icon="stop" />
         </button>
@@ -27,6 +36,7 @@
 </template>
 
 <script>
+import { servoControl } from '../../api'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBox, faArrowUp, faArrowDown, faStopCircle } from '@fortawesome/free-solid-svg-icons'
 import { computed } from 'vue'
@@ -36,16 +46,28 @@ export default {
     FontAwesomeIcon
   },
   setup () {
+    const SERVO_ACTION = {
+      UP: 'SERVO_UP',
+      DOWN: 'SERVO_DOWN',
+      STOP: 'SERVO_STOP'
+    }
     const box = computed(() => faBox)
     const arrowUp = computed(() => faArrowUp)
     const arrowDown = computed(() => faArrowDown)
     const stop = computed(() => faStopCircle)
 
+    const handleServo = (action) => {
+      if (action === 'UP') servoControl(SERVO_ACTION.UP)
+      if (action === 'DOWN') servoControl(SERVO_ACTION.DOWN)
+      if (action === 'STOP') servoControl(SERVO_ACTION.STOP)
+    }
+
     return {
       box,
       arrowUp,
       arrowDown,
-      stop
+      stop,
+      handleServo
     }
   }
 }
@@ -55,6 +77,9 @@ export default {
   height: 100%;
   >.tab-container{
     height: 100%;
+    >div {
+      height: 100%;
+    }
   }
 }
 </style>
