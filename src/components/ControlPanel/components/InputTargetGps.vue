2459a4c4240a49f41dfb0883b1c6e0ddcd693200
Tony Chung <waiting33118@gmail.com>
2021-02-24 18:53:28 +0800

    feat: edit component input group gps coordinate

--- src/components/ControlPanel/components/InputTargetGps.vue
+++ src/components/ControlPanel/components/InputTargetGps.vue
@@ -1,53 +1,46 @@
 <template>
-  <div class="d-flex justify-content-center px-2 py-2">
-    <form class="input-group d-flex align-items-center">
-      <FontAwesomeIcon
-        :icon="mapMark"
-        class="me-2"
-        style="color:blue"
-      />
-      <span class="input-group-text">Lng</span>
-      <input
-        v-model="coords.lng"
-        type="text"
-        class="form-control text-center"
-        disabled
-        readonly
-      >
-      <span class="input-group-text">Lat</span>
-      <input
-        v-model="coords.lat"
-        type="text"
-        class="form-control text-center"
-        disabled
-        readonly
-      >
-    </form>
+  <div class="input-wrapper">
+    <el-input
+      v-model="coords.lng"
+      prefix-icon="el-icon-location"
+      :disabled="true"
+    >
+      <template #prepend>
+        Lng
+      </template>
+    </el-input>
+
+    <el-input
+      v-model="coords.lat"
+      prefix-icon="el-icon-location"
+      :disabled="true"
+    >
+      <template #prepend>
+        Lat
+      </template>
+    </el-input>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
 import { computed } from 'vue'
 import { useStore } from 'vuex'
 export default {
   name: 'InputTargetGps',
-  components: {
-    FontAwesomeIcon
-  },
   setup () {
     const store = useStore()
-    const mapMark = computed(() => faMapMarkedAlt)
     const coords = computed(() => store.getters['Drone/getTargetGps'])
     return {
-      mapMark,
       coords
     }
   }
 }
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+.input-wrapper {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
