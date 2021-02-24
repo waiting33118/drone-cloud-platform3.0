bec17e6f6c3183b65be54d1235536e15589cb9b6
Tony Chung <waiting33118@gmail.com>
2021-02-24 18:44:31 +0800

    feat: import element-ui plus UI library

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/lib/theme-chalk/display.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
