984d6a44bf0a6c6cfb5c1004da8d7422156b18bd
Tony Chung <waiting33118@gmail.com>
2021-02-24 18:52:31 +0800

    feat: edit component slider gimbal X

--- src/components/ControlPanel/components/InputGimbalX.vue
+++ src/components/ControlPanel/components/InputGimbalX.vue
@@ -1,26 +1,14 @@
 <template>
-  <div class="d-flex flex-column align-items-center p-2">
-    <label for="inputGimbalX">
-      <FontAwesomeIcon :icon="video" />
-      Gimbal Left & Right
-    </label>
-
-    <input
-      id="inputGimbalX"
+  <div class="slider-wrapper">
+    <label for="slider"><FontAwesomeIcon :icon="video" />Gimbal Left & Right</label>
+    <el-slider
+      id="slider"
       v-model="pwmStatus"
-      type="range"
-      class="form-range"
-      min="1200"
-      max="1800"
+      :min="1200"
+      :max="1800"
+      :marks="marks"
       @change="handleChange"
-    >
-    <input
-      v-model="pwmStatus"
-      type="text"
-      class="form-control text-center"
-      disabled
-      readonly
-    >
+    />
   </div>
 </template>
 
@@ -36,18 +24,44 @@ export default {
     FontAwesomeIcon
   },
   setup () {
+    const marks = {
+      1200: {
+        style: {
+          color: '#1989FA'
+        },
+        label: '1200(Left)'
+      },
+      1500: {
+        style: {
+          color: '#1989FA'
+        },
+        label: '1500(Center)'
+      },
+      1800: {
+        style: {
+          color: '#1989FA'
+        },
+        label: '1800(Right)'
+      }
+    }
     const store = useStore()
     const video = computed(() => faVideo)
     const pwmStatus = computed({
       get: () => store.getters['Drone/getGimbalX'],
-      set: pwm => store.dispatch('Drone/setGimbalX', Number(pwm))
+      set: pwm => store.dispatch('Drone/setGimbalX', pwm)
     })
     const handleChange = () => gimbalControl('GIMBAL_LEFT_RIGHT', pwmStatus.value)
     return {
       video,
       pwmStatus,
-      handleChange
+      handleChange,
+      marks
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.slider-wrapper {
+  text-align: center;
+}
+</style>
