47af0953bb076770507799da7c9480e2141ef49c
Tony Chung <waiting33118@gmail.com>
2021-03-13 16:04:05 +0800

    feat: add User modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import Drone from './Drone'
+import User from './User'
 
 export default createStore({
   state: {
@@ -11,6 +12,7 @@ export default createStore({
   getters: {
   },
   modules: {
-    Drone
+    Drone,
+    User
   }
 })
