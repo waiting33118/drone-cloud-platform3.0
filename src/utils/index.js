fbd843332daa6568eb9a8adcfdf27b490cf6df9b
Tony Chung <waiting33118@gmail.com>
2021-03-13 16:15:08 +0800

    feat: export notification feature

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,8 @@ import { socketInit } from './socket'
 import { logFormat, messageParse } from './logFormatter'
 import { fetchLocation } from './fetchLocation'
 import { alert, gotoMissionConfirm } from './sweetAlert'
-import { instance } from './axios'
+import notify from './notification'
+import instance from './axios'
 
 export const useSocket = socketInit
 export const uselogFormatter = logFormat
@@ -10,4 +11,5 @@ export const useMessageParse = messageParse
 export const getUserLocation = fetchLocation
 export const useGotoMissionConfirm = gotoMissionConfirm
 export const useAlert = alert
+export const useNotify = notify
 export const customAxios = instance
