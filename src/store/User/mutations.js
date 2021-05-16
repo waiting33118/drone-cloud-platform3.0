f7012eaff68154cb7d6561f15b064e0dcf132a7f
Tony Chung <waiting33118@gmail.com>
2021-05-16 22:29:16 +0800

    feat: remove signin mutation

--- src/store/User/mutations.js
+++ src/store/User/mutations.js
@@ -1,19 +1,10 @@
 export default {
-  signIn (state, { user }) {
+  setUserInfo (state, user) {
     state.isAuthenticated = true
     state.user = { ...user }
   },
   signOut (state) {
     state.isAuthenticated = false
-    state.user = {
-      id: '',
-      name: '',
-      email: '',
-      droneId: ''
-    }
-  },
-  setUserInfo (state, { user }) {
-    state.isAuthenticated = true
-    state.user = { ...user }
+    state.user = {}
   }
 }
