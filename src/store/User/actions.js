dd7ef095e44ba0f89a6a0e02f92f4a205edd137e
Tony Chung <waiting33118@gmail.com>
2021-05-16 22:28:58 +0800

    feat: remove signin action

--- src/store/User/actions.js
+++ src/store/User/actions.js
@@ -1,14 +1,9 @@
-import { auth } from '../../api'
-
 export default {
-  signIn ({ commit }, { user }) {
-    commit('signIn', { user })
+  setUserInfo ({ commit }, user) {
+    commit('setUserInfo', user)
   },
   signOut ({ commit }) {
     commit('signOut')
-  },
-  async fetchUserInfo ({ commit }) {
-    const user = await auth.fetchUserInfo()
-    commit('setUserInfo', { user })
   }
+
 }
