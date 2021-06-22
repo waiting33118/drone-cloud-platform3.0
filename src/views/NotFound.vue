63a62a763aea668bcbed8ccf426460f6f3caa59f
Tony Chung <waiting33118@gmail.com>
2021-06-22 13:59:20 +0800

    feat: create notfound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <img src="../assets/404.jpg" alt="not-found" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  width: 100%;
+  height: calc(100vh - 60px);
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 500px;
+    height: 500px;
+  }
+}
+</style>
