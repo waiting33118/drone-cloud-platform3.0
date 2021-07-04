c6e828ac6a50cec9016c1e8fb00a35d002477da0
Tony Chung <waiting33118@gmail.com>
2021-07-04 15:29:50 +0800

    feat: create bidirectional peer connection negotiation feature

--- src/components/Stream/Stream.vue
+++ src/components/Stream/Stream.vue
@@ -1,68 +1,150 @@
 <template>
-  <video poster="../../assets/live-stream.png" muted autoplay></video>
+  <div class="video__wrapper">
+    <video
+      ref="remoteVideoEl"
+      poster="../../assets/live-stream.png"
+      autoplay
+    ></video>
+    <a-button
+      class="video__button"
+      size="small"
+      shape="round"
+      type="primary"
+      @click="startPeerNegotiation"
+      >Connect</a-button
+    >
+  </div>
 </template>
 
 <script>
 import socket from '../../lib/websocket'
-import WebRTC from '../../lib/webRTC'
-import { onBeforeUnmount, onMounted } from '@vue/runtime-core'
+import {
+  createPeerConnection,
+  createAnswerAndSetLocalSDP,
+  createOfferAndSetLocalSDP,
+  getLocalStream
+} from '../../lib/webRTC'
+import { onBeforeUnmount, ref } from '@vue/runtime-core'
 import { useStore } from 'vuex'
+import { message } from 'ant-design-vue'
 export default {
   name: 'Stream',
   setup() {
+    const remoteVideoEl = ref(null)
+    let pc
+    let localStream
     const store = useStore()
-    const webRTC = new WebRTC()
-
     const setLogs = (log) => store.dispatch('setLogs', log)
-
-    webRTC.createConnection()
-
-    socket.on('webrtc-topic', async (data) => {
-      if (data.type === 'offer') {
-        await webRTC.setRemoteSDP(data.payload)
-        setLogs('Recieved offer')
-        const answer = await webRTC.createAnswerAndSetLocalSDP()
-        setLogs('Send answer')
-        socket.emit('send-webrtc', { type: 'answer', payload: answer })
+    const onIceCandidate = (event) => {
+      if (event.candidate) {
+        socket.emit('send-webrtc', {
+          type: 'candidate',
+          payload: event.candidate
+        })
+        setLogs('Send candidate')
       }
-
-      if (data.type === 'candidate') {
-        setLogs('Recieved candidate')
-        await webRTC.addCandidate(data.payload)
+    }
+    const onTrack = (event) => {
+      setLogs('Received track')
+      remoteVideoEl.value.srcObject = new MediaStream()
+      remoteVideoEl.value.srcObject = event.streams[0]
+    }
+    const onIceConnectionStateChange = (event) => {
+      const status = event.target.iceConnectionState
+      setLogs(`ICE connection Change: ${status}`)
+      if (status === 'disconnected') {
+        remoteVideoEl.value.srcObject = new MediaStream()
       }
-    })
+    }
 
-    onMounted(async () => {
-      const handleOnIceCnadidate = ({ candidate }) => {
-        if (candidate) {
-          setLogs('Send candidate')
-          socket.emit('send-webrtc', { type: 'candidate', payload: candidate })
-        }
+    const initPeerConnection = () => {
+      pc = createPeerConnection()
+      setLogs('Create peer connection')
+      pc.onicecandidate = onIceCandidate
+      pc.ontrack = onTrack
+      pc.oniceconnectionstatechange = onIceConnectionStateChange
+      if (localStream) {
+        localStream.getTracks().forEach((track) => pc.addTrack(track))
+        setLogs('Add local tracks to peer connection')
       }
-      const handleOnTrack = (event) => {
-        if (event.track.kind === 'video') {
-          setLogs('Recieved track')
-          document.querySelector('video').srcObject = event.streams[0]
-        }
-      }
+    }
 
-      webRTC.setIceCandidateListener(handleOnIceCnadidate)
-      webRTC.setTrackListener(handleOnTrack)
-      // await webRTC.getLocalStream()
-      // webRTC.addTrackToConnection()
-    })
+    const startPeerNegotiation = async () => {
+      initPeerConnection()
+      const offer = await createOfferAndSetLocalSDP(pc)
+      setLogs('Create offer & set offer becomes local SDP')
+      socket.emit('send-webrtc', { type: 'offer', payload: offer })
+      setLogs('Send offer')
+    }
+
+    getLocalStream()
+      .then((mediaStream) => {
+        localStream = mediaStream
+      })
+      .catch((error) => {
+        message.error(
+          `Cannot add local stream to peer, cause of ${error.message}`
+        )
+      })
+      .finally(() => {
+        socket.on('webrtc-topic', async (data) => {
+          if (data.type === 'offer') {
+            setLogs('Received offer')
+            initPeerConnection()
+            await pc.setRemoteDescription(data.payload)
+            setLogs('Set offer becomes remote SDP')
+            const answer = await createAnswerAndSetLocalSDP(pc)
+            setLogs('Create answer & set answer becomes local SDP')
+            socket.emit('send-webrtc', { type: 'answer', payload: answer })
+            setLogs('Send answer')
+          }
+
+          if (data.type === 'answer') {
+            setLogs('Received answer')
+            await pc.setRemoteDescription(data.payload)
+            setLogs('Set answer becomes remote SDP')
+          }
+
+          if (data.type === 'candidate') {
+            await pc.addIceCandidate(data.payload)
+            setLogs('Add received candidate')
+          }
+        })
+      })
 
     onBeforeUnmount(() => {
-      // webRTC.stopStream()
+      socket.off('webrtc-topic')
+      setTimeout(() => {
+        if (localStream) {
+          localStream.getTracks().forEach((track) => track.stop())
+        }
+      }, 1000)
     })
+
+    return {
+      remoteVideoEl,
+      startPeerNegotiation
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-video {
-  width: 100%;
+.video__wrapper {
   height: 100%;
-  object-fit: cover;
+  position: relative;
+
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .video__button {
+    position: absolute;
+    top: 1px;
+    right: 1px;
+    z-index: 110;
+  }
 }
 </style>
