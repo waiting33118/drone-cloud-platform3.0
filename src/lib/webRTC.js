3aafc3ff43dd6aea890f5dc80c89103b878a3c9b
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 14:15:06 +0800

    docs: Library/WebRTC

--- src/lib/webRTC.js
+++ src/lib/webRTC.js
@@ -1,28 +1,39 @@
 /**
+ * Initial peer connection instance
  * @returns {RTCPeerConnection}
  */
 export const createPeerConnection = () => {
   return new RTCPeerConnection({
     iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-  })
-}
+  });
+};
 
 /**
+ * Callee create answer and set as local SDP
  * @param {RTCPeerConnection} pc
  * @returns {Promise<RTCSessionDescriptionInit>} answer
  */
 export const createAnswerAndSetLocalSDP = async (pc) => {
-  const answer = await pc.createAnswer()
-  await pc.setLocalDescription(answer)
-  return answer
-}
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+  return answer;
+};
 
+/**
+ * Caller create offer and set as local SDP
+ * @param {RTCPeerConnection} pc
+ * @returns {Promise<RTCSessionDescriptionInit>} offer
+ */
 export const createOfferAndSetLocalSDP = async (pc) => {
-  const offer = await pc.createOffer()
-  await pc.setLocalDescription(offer)
-  return offer
-}
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  return offer;
+};
 
+/**
+ * Get media stream
+ * @returns {Promise<MediaStream>}
+ */
 export const getLocalStream = () => {
   return navigator.mediaDevices.getUserMedia({
     video: {
@@ -35,5 +46,5 @@ export const getLocalStream = () => {
       noiseSuppression: true,
       autoGainControl: true
     }
-  })
-}
+  });
+};
