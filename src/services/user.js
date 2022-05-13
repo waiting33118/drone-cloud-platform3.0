6197e4ea684b65235aba70787ec9f44f9548ccd0
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 11:12:07 +0800

    docs: APIs of user

--- src/services/user.js
+++ src/services/user.js
@@ -1,10 +1,16 @@
-import axios from '../lib/axios'
+/**
+ * Auth APIs
+ *
+ * - User info
+ * - Edit drone ID
+ */
+import axios from '../lib/axios';
 
 export default {
   async getUserInfo() {
-    return await axios.get('/user/me')
+    return await axios.get('/user/me');
   },
   async editUserDroneId(droneId) {
-    return await axios.post('/user/droneId', droneId)
+    return await axios.post('/user/droneId', droneId);
   }
-}
+};
