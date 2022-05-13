f179c485ac8e0f06a47f203aa47479a28488dd75
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 11:11:38 +0800

    docs: APIs of auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,24 @@
-import axios from '../lib/axios'
+/**
+ * Auth APIs
+ *
+ * - Sign up
+ * - Sign in
+ * - Refresh token (every 4 minutes)
+ * - Log out
+ */
+import axios from '../lib/axios';
 
 export default {
   async signup(payload) {
-    return await axios.post('/auth/signup', payload)
+    return await axios.post('/auth/signup', payload);
   },
   async login(payload) {
-    return await axios.post('/auth/login', payload)
+    return await axios.post('/auth/login', payload);
   },
   async refreshToken() {
-    await axios.get('/auth/token')
+    await axios.get('/auth/token');
   },
   async logout() {
-    await axios.post('/auth/logout')
+    await axios.post('/auth/logout');
   }
-}
+};
