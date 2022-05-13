e0a6934bd3c477f94a6955cb699aafab60637104
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 14:14:56 +0800

    docs: Library/geolocation

--- src/lib/geolocation.js
+++ src/lib/geolocation.js
@@ -1,16 +1,17 @@
 /**
+ * Get user's current geolocation
  * @returns {Promise<[number,number]}
  */
 export const getUserCurrentLocation = () => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
       ({ coords }) => {
-        resolve([coords.longitude, coords.latitude])
+        resolve([coords.longitude, coords.latitude]);
       },
       (err) => {
-        reject(err.message)
+        reject(err.message);
       },
       { enableHighAccuracy: true }
-    )
-  })
-}
+    );
+  });
+};
