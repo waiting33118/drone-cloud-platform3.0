07aa4cb1f4a927a8fba1dee337601e73abcec28b
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 11:11:56 +0800

    docs: APIs of flight record

--- src/services/drone.js
+++ src/services/drone.js
@@ -1,19 +1,26 @@
-import axios from '../lib/axios'
+/**
+ * Drone record APIs
+ *
+ * - Save flight record
+ * - Get flight records
+ * - Get specific flight mission record
+ */
+import axios from '../lib/axios';
 
 export default {
   /**
    * @param {Array} records
    */
   async saveFlightRecords(records) {
-    return await axios.post('/drone/records', { records })
+    return await axios.post('/drone/records', { records });
   },
   async getFlightRecords() {
-    return await axios.get('/drone/records')
+    return await axios.get('/drone/records');
   },
   /**
    * @param {string} id
    */
   async getFlightRecord(id) {
-    return await axios.get(`/drone/records/${id}`)
+    return await axios.get(`/drone/records/${id}`);
   }
-}
+};
