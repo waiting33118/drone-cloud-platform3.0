49a92d1d901f0424b5bc67fba7de9ec2463a1e02
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 11:10:15 +0800

    docs: Views/Drone

--- src/views/Drone.vue
+++ src/views/Drone.vue
@@ -13,40 +13,43 @@
 </template>
 
 <script>
-import Stream from '../components/Stream/Stream.vue'
-import Mapbox from '../components/Mapbox/Mapbox.vue'
-import Control from '../components/Control/Control.vue'
-import socket from '../lib/websocket'
-import { useStore } from 'vuex'
-import { message } from 'ant-design-vue'
-import { computed, onBeforeUnmount } from '@vue/runtime-core'
+import Stream from '../components/Stream/Stream.vue';
+import Mapbox from '../components/Mapbox/Mapbox.vue';
+import Control from '../components/Control/Control.vue';
+import socket from '../lib/websocket';
+import { useStore } from 'vuex';
+import { message } from 'ant-design-vue';
+import { computed, onBeforeUnmount } from '@vue/runtime-core';
 export default {
   name: 'Drone',
   components: { Stream, Mapbox, Control },
   setup() {
-    const store = useStore()
-    const rabbitmqIsInit = computed(() => store.getters.getRabbitmqIsInit)
-    const user = computed(() => store.getters.getUserInfo)
-    const saveLogs = (log) => store.dispatch('setLogs', log)
+    const store = useStore();
+    const rabbitmqIsInit = computed(() => store.getters.getRabbitmqIsInit);
+    const user = computed(() => store.getters.getUserInfo);
+    const saveLogs = (log) => store.dispatch('setLogs', log);
 
+    // RabbitMQ queues establishment
     const rabbitmqInit = () => {
-      saveLogs(`Websocket connected: ${socket.id}`)
-      saveLogs(`Drone ID: ${user.value.droneId}`)
-      socket.emit('establish-rabbitmq-connection', user.value.droneId)
-    }
+      saveLogs(`Websocket connected: ${socket.id}`);
+      saveLogs(`Drone ID: ${user.value.droneId}`);
+      socket.emit('establish-rabbitmq-connection', user.value.droneId);
+    };
 
+    // Trigger RabbitMQ when the first come or refresh pages
     if (!rabbitmqIsInit.value) {
-      rabbitmqInit()
-      store.dispatch('setRabbitmqIsInit', true)
+      rabbitmqInit();
+      store.dispatch('setRabbitmqIsInit', true);
     }
 
-    socket.on('connect', () => rabbitmqInit())
+    // Websocket event listening
+    socket.on('connect', () => rabbitmqInit());
     socket.on('disconnect', (reason) => {
-      saveLogs(`Websocket disconnected: ${reason}`)
-    })
+      saveLogs(`Websocket disconnected: ${reason}`);
+    });
     socket.on('queue-created', (queueName) => {
-      saveLogs(`Queue created: ${queueName}`)
-    })
+      saveLogs(`Queue created: ${queueName}`);
+    });
     socket.on('drone-topic', (data) => {
       if (data.type === 'message') {
         const {
@@ -64,7 +67,7 @@ export default {
             },
             speed: { air_speed: speed }
           }
-        } = data
+        } = data;
         const droneInfo = {
           timeStamp,
           roll,
@@ -81,41 +84,42 @@ export default {
           longitude,
           altitude,
           speed
-        }
-        store.dispatch('drone/setDroneInfo', droneInfo)
+        };
+        store.dispatch('drone/setDroneInfo', droneInfo);
       }
 
       if (data.type === 'cmd_ack') {
         if (data.cmd_result.includes('ACCEPTED')) {
-          message.success(data.cmd_result)
+          message.success(data.cmd_result);
         } else {
-          message.error(data.cmd_result)
+          message.error(data.cmd_result);
         }
-        saveLogs(data.cmd)
+        saveLogs(data.cmd);
       }
 
       if (data.type === 'mission_ack') {
         if (data.mission_result.includes('ACCEPTED')) {
-          message.success(data.mission_result)
+          message.success(data.mission_result);
         } else {
-          message.error(data.mission_result)
+          message.error(data.mission_result);
         }
-        saveLogs(data.mission_result)
+        saveLogs(data.mission_result);
       }
 
       if (data.type === 'apm_text') {
-        saveLogs(data.text)
+        saveLogs(data.text);
       }
-    })
+    });
 
+    // Remove listener to prevent multiple listening
     onBeforeUnmount(() => {
-      socket.off('connect')
-      socket.off('disconnect')
-      socket.off('queue-created')
-      socket.off('drone-topic')
-    })
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('queue-created');
+      socket.off('drone-topic');
+    });
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
