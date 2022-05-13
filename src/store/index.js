731447318230494e6f5bd46fc63840a624300251
Tony Chung <47709206+waiting33118@users.noreply.github.com>
2022-05-13 11:11:11 +0800

    docs: Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from 'vuex'
-import drone from './drone'
+import { createStore } from 'vuex';
+import drone from './drone';
 
+// Global states
 export default createStore({
   state: {
     logs: [],
@@ -19,68 +20,69 @@ export default createStore({
         id: Date.now(),
         timeStamp: new Date().toLocaleString(),
         payload
-      })
+      });
     },
     clearLogs({ commit }) {
-      commit('clearLogs')
+      commit('clearLogs');
     },
     setUserInfo({ commit }, payload) {
-      commit('setUserInfo', payload)
+      commit('setUserInfo', payload);
     },
     setIsAuth({ commit }, payload) {
-      commit('setIsAuth', payload)
+      commit('setIsAuth', payload);
     },
     setRabbitmqIsInit({ commit }, payload) {
-      commit('setRabbitmqIsInit', payload)
+      commit('setRabbitmqIsInit', payload);
     },
     setRabbitmqAdminIsInit({ commit }, payload) {
-      commit('setRabbitmqAdminIsInit', payload)
+      commit('setRabbitmqAdminIsInit', payload);
     }
   },
   mutations: {
     setLogs(state, payload) {
-      state.logs.unshift(payload)
+      state.logs.unshift(payload);
     },
     clearLogs(state) {
-      state.logs = []
+      state.logs = [];
     },
     setUserInfo(state, payload) {
-      state.user = payload
+      state.user = payload;
     },
     setIsAuth(state, payload) {
-      state.isAuthenticated = payload
+      state.isAuthenticated = payload;
     },
     setRabbitmqIsInit(state, payload) {
-      state.rabbitmqIsInit = payload
+      state.rabbitmqIsInit = payload;
     },
     setRabbitmqAdminIsInit(state, payload) {
-      state.rabbitmqAdminIsInit = payload
+      state.rabbitmqAdminIsInit = payload;
     }
   },
   getters: {
     getLogs(state) {
-      return state.logs
+      return state.logs;
     },
     getUserInfo(state) {
-      return state.user
+      return state.user;
     },
     getUsername(state) {
-      return state.user.email.substr(0, state.user.email.indexOf('@'))
+      return state.user.email.substr(0, state.user.email.indexOf('@'));
     },
     getIsAuth(state) {
-      return state.isAuthenticated
+      return state.isAuthenticated;
     },
     getIsAdmin(state) {
-      return state.user.isAdmin
+      return state.user.isAdmin;
     },
     getRabbitmqIsInit(state) {
-      return state.rabbitmqIsInit
+      return state.rabbitmqIsInit;
     },
     getRabbitmqAdminIsInit(state) {
-      return state.rabbitmqAdminIsInit
+      return state.rabbitmqAdminIsInit;
     }
   },
   modules: {
+    // Dedicate modules for drone-related
     drone
   }
-})
+});
